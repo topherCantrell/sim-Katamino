936f1a1985d7affd5c68d27b567b0117be250db6
Christopher Cantrell <topherCantrell@gmail.com>
2019-02-27 16:07:46 -0600

    updates

--- src/app_combinations.py
+++ src/app_combinations.py
@@ -111,7 +111,8 @@ def solve_all_combos(num):
 # solve_all_combos(7)
 # solve_all_combos(8)
 # solve_all_combos(9)
+# solve_all_combos(10)
 
-solve_all_combos(10)
-# ? solve_all_combos(11)
+solve_all_combos(11)
+
 # ? solve_all_combos(12)
